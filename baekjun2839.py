da58b0c59bbfdf0c37fd6329b06ea539418cb987
baekinwon <lbrlub2@gmail.com>
2023-03-16 17:50:03 +0900

    2839

--- baekjun2839.py
+++ baekjun2839.py
@@ -24,37 +24,3 @@ else:
         print(-1)
 
 
-
-# kg_5 = 0
-# kg_3 = 0
-
-# kg_change = kg
-
-# if kg_change % 5  == 0:
-#     print(kg_change//5)
-# else:
-#     while(True):
-#         if kg_change >= 5:
-#             kg_change -= 5
-#             kg_5+=1
-#             if kg_change % 3==0 and kg_change % 5 <3:
-#                 break
-#         else:
-#             if kg_change % 3 !=0:
-#                 if kg_5 !=0:
-#                     kg_5-=1
-#                 kg_change+=5
-#             break
-
-#     while(True):
-#         if kg_change >= 3:
-#             kg_change -=3
-#             kg_3+=1
-#         else:
-#             break
-#     print(kg_5)
-#     print(kg_3)
-#     if kg_5 * 5 + kg_3 * 3 == kg:
-#         print(kg_5+kg_3)
-#     else:
-#         print(-1)        
